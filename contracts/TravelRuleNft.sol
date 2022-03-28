f81ea8bc6d048b5fe771f03b59d2896216fc3d0f
hyunkicho <dmsrlgusrl@naver.com>
2022-03-29 04:46:00 +0900

    build: NFT 예시 코드 및 배포 코드

--- contracts/TravelRuleNft.sol
+++ contracts/TravelRuleNft.sol
@@ -1,23 +1,31 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract TravelRuleNft is ERC721 {
-    string private greeting;
+contract TravelRuleNft is ERC721URIStorage {
+     using Strings for uint256;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    string private _tokenUri;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        string memory _baseUri
+    ) ERC721(name, symbol) {
+        _tokenUri = _baseUri;
+    }
+ 
+    function mint(address user, uint256 tokenId) public {
+        _setTokenURI(tokenId, _tokenUri);
+        _mint(user, tokenId);
+    }
+    
+    function burn(uint256 tokenId) public  {
+        _setTokenURI(tokenId, "");
+        _burn(tokenId);
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
 }
+
